f58d68037421b9b09291d1b74b2ad1bac075a8a6
magat <mathieu.agar@gmail.com>
2015-07-08 00:59:23 +0200

    indentation

--- demo.js
+++ demo.js
@@ -17,49 +17,49 @@ circle.beginFill(0x450000).drawCircle(INIT_CIRCLE_X, INIT_CIRCLE_Y, CIRCLE_RADIU
 background.addChild(circle);
 
 function keyboard() {
-  var kb = {left: 37, up: 38, right: 39, down: 40};
-  kb.pressed = {};
-  kb.pressed[kb.left] = false;
-  kb.pressed[kb.up] = false;
-  kb.pressed[kb.right] = false;
-  kb.pressed[kb.down] = false;
-  kb.press = undefined;
-  kb.release = undefined;
+	var kb = {left: 37, up: 38, right: 39, down: 40};
+	kb.pressed = {};
+	kb.pressed[kb.left] = false;
+	kb.pressed[kb.up] = false;
+	kb.pressed[kb.right] = false;
+	kb.pressed[kb.down] = false;
+	kb.press = undefined;
+	kb.release = undefined;
 
-  //The `downHandler`
-  var downHandler = function(event) {
-    if (event.keyCode >= this.left && event.keyCode <= this.down) {
+	//The `downHandler`
+	var downHandler = function(event) {
+		if (event.keyCode >= this.left && event.keyCode <= this.down) {
 			if(!this.pressed[event.keyCode] && this.press) this.press(event.keyCode);
 			this.pressed[event.keyCode] = true;
-    }
-    event.preventDefault();
-  };
+		}
+		event.preventDefault();
+	};
 
-  //The `upHandler`
-  var upHandler = function(event) {
-    if (event.keyCode >= this.left && event.keyCode <= this.down) {
+	//The `upHandler`
+	var upHandler = function(event) {
+		if (event.keyCode >= this.left && event.keyCode <= this.down) {
 			if(this.pressed[event.keyCode] && this.release) this.release(event.keyCode);
 			this.pressed[event.keyCode] = false;
-    }
-    event.preventDefault();
-  };
+		}
+		event.preventDefault();
+	};
 
-  //Attach event listeners
-  window.addEventListener(
-    "keydown", downHandler.bind(kb), false
-  );
-  window.addEventListener(
-    "keyup", upHandler.bind(kb), false
-  );
-  return kb;
+	//Attach event listeners
+	window.addEventListener(
+			"keydown", downHandler.bind(kb), false
+			);
+	window.addEventListener(
+			"keyup", upHandler.bind(kb), false
+			);
+	return kb;
 }
 
 var ctrl = keyboard();
 
 function loop(){
-  requestAnimationFrame(loop);
-  nextFrame();
-  renderer.render(stage);
+	requestAnimationFrame(loop);
+	nextFrame();
+	renderer.render(stage);
 }
 
 
@@ -74,12 +74,12 @@ var GROUND = 580 - (INIT_CIRCLE_Y + CIRCLE_RADIUS / 2);
 var WALL_LEFT =  - INIT_CIRCLE_X + CIRCLE_RADIUS;
 var WALL_RIGHT = 800 - (INIT_CIRCLE_X + CIRCLE_RADIUS);
 function nextFrame(){
- 
+
 	vx = 0; 
 	// gravity applies if position > bottom
-  if(circle.position.y < GROUND){
-    vy += g;
-  } else {
+	if(circle.position.y < GROUND){
+		vy += g;
+	} else {
 		// if on the ground, no gravity
 		vy = 0;
 		// jumping only applies when on the ground
@@ -100,8 +100,8 @@ function nextFrame(){
 		}
 	}
 	// Update the circle position according to new velocity
-  circle.position.x += vx;
-  circle.position.y += vy;
+	circle.position.x += vx;
+	circle.position.y += vy;
 
 	if(circle.position.x < WALL_LEFT){
 		circle.position.x = WALL_LEFT;
