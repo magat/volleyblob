1be8069d237ea4a917177222ddbfd500e6017331
magat <mathieu.agar@gmail.com>
2015-07-13 17:01:16 +0200

    Move bouncing out of the ball class

--- ball.js
+++ ball.js
@@ -9,54 +9,22 @@ function Ball(avatar){
     x: 0,
     y: 0
   };
-  this.push = {
-		x: false,
-		y: false
-	};
 }
 
+Ball.prototype.push = function(x, y){
+  this.direction.x = x;
+  this.direction.y = y;
+};
+
 Ball.prototype.refresh = function(){
-  var pos = this.avatar.position;
-  var v = this.velocity;
-
-	// gravity applies if position > bottom
-	if(pos.y < GROUND){
-    v.y += gravity;
-	} else {
-    v.y = 0;
-    // on the ground, lose horizontal speed
-    v.x = 0.75 * v.x;
-  }
-
   // apply the direction to the velocity
-  if(this.push.x){
-    v.x += this.direction.x * jump;
-    this.push.x = false;
-  }
-  if(this.push.y){
-    v.y += this.direction.y * jump;
-    this.push.y = false;
-	}
-	// compute the new avatar position according to new velocity
-	var x = pos.x + v.x;
-  var	y = pos.y + v.y;
+  this.velocity.x += this.direction.x;
+  this.velocity.y += this.direction.y;
 
-  if(x <= WALL_LEFT){
-    x = WALL_LEFT;
-    this.direction.x = Math.abs(this.direction.x) * 0.75;
-    this.push.x = true;
-  }
-  if(x >= WALL_RIGHT){
-    x = WALL_RIGHT;
-    this.direction.x = - Math.abs(this.direction.x) * 0.75;
-    this.push.x = true;
-  }
-  if(y >= GROUND){
-    y = GROUND;
-    this.direction.y = v.y > 5 ? (- Math.abs(v.y) * 0.75 / jump) : 0;
-    this.push.y = true;
-  } 
+  // apply the velocity to the position
+  this.avatar.position.x += this.velocity.x;
+  this.avatar.position.y += this.velocity.y;
 
-	pos.x = x;
-	pos.y = y;
+  // reset direction data
+  this.push(0,0);
 }
--- demo.js
+++ demo.js
@@ -1,59 +1,80 @@
-var INIT_CIRCLE_X = 400;
-var INIT_CIRCLE_Y = 300;
-var CIRCLE_RADIUS = 20;
-var GROUND = 580 - (INIT_CIRCLE_Y + CIRCLE_RADIUS / 2);
-var WALL_LEFT =  - INIT_CIRCLE_X + CIRCLE_RADIUS;
-var WALL_RIGHT = 800 - (INIT_CIRCLE_X + CIRCLE_RADIUS);
+var W = 800;
+var H = 600;
+var BALL_RADIUS = 20;
+var GROUND_THICKNESS = 10;
+var GRAVITY = 1;
 
+// under this speed, the ball won't bounce on the floor
+var BOUNCE_SPEED = 5;
+// factor makes the vertical speed after a bounce
+var BOUNCE_FACTOR = 0.605;
 
-var renderer = new PIXI.autoDetectRenderer(800,600, {backgroundColor: 0xFFFFFF});
+var renderer = new PIXI.autoDetectRenderer(W, H, {backgroundColor: 0xFFFFFF});
 var stage = new PIXI.Container();
-// the sky and ground
-var background = new PIXI.Graphics();
-background.beginFill(0x00DD88).drawRect(0, 590, 800, 10).endFill();
-background.beginFill(0x9AFEFF).drawRect(0, 0, 800, 590).endFill(); 
-stage.addChild(background);
+// the sky 
+var sky = new PIXI.Graphics();
+sky.beginFill(0x9AFEFF).drawRect(0, 0, W, H).endFill(); 
+stage.addChild(sky);
+// the ground
+var ground = new PIXI.Graphics();
+ground.beginFill(0x00DD88).drawRect(0, 0, W, GROUND_THICKNESS).endFill();
+ground.position.y = H - GROUND_THICKNESS;
+sky.addChild(ground);
 
 document.body.appendChild(renderer.view);
 
 var circle = new PIXI.Graphics();
-circle.beginFill(0x450000).drawCircle(INIT_CIRCLE_X, INIT_CIRCLE_Y, CIRCLE_RADIUS).endFill();
-background.addChild(circle);
+circle.beginFill(0x450000).drawCircle(0, 0, BALL_RADIUS).endFill();
+circle.position.x = W/2;
+circle.position.y = H/2;
+sky.addChild(circle);
 
 //var p1 = new Player(circle);
 var ball = new Ball(circle);
 var kb = keyboard();
-function push(ball, axis, val){
-	ball.push[axis] = true;
-	ball.direction[axis] = val;
-}
-kb.press = function(key){
-	switch(key){
-		case this.up:
-			push(ball, "y", -1);
-			break;
-		case this.down:
-			push(ball, "y", 1);
-			break;
-		case this.left:
-			push(ball, "x", -1);
-			break;
-		case this.right:
-			push(ball, "x", 1);
-			break;
-	}
 
-}
-function loop(){
-	requestAnimationFrame(loop);
-
-	ball.refresh();
-
-	renderer.render(stage);
-}
-
-var gravity = 1;
 var jump = 15;
 var speed = 5;
+
+kb.press = function(key){
+  switch(key){
+    case this.up:
+      ball.push(0, -jump);
+      break;
+    case this.down:
+      ball.push(0, jump);
+      break;
+    case this.left:
+      ball.push(-jump, 0);
+      break;
+    case this.right:
+      ball.push(jump, 0);
+      break;
+  }
+}
+
+function handleBallCollisions(){
+  //TODO on the ground, lose horizontal speed
+  // ball bounces on the floor
+  if(circle.position.y >= (ground.y - BALL_RADIUS)){
+    circle.position.y = ground.y - BALL_RADIUS;
+    if(ball.velocity.y > 5){
+      ball.push(0, -ball.velocity.y * (1 + BOUNCE_FACTOR));
+    } 
+  } else {
+    //if not on the floor, gravity applies
+    ball.push(0, GRAVITY);
+  }
+}
+
+function loop(){
+  requestAnimationFrame(loop);
+
+  // compute new ball position
+  ball.refresh();
+  handleBallCollisions();
+
+  renderer.render(stage);
+}
 
 loop();
