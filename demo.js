e119764322864ca0a3fe2240b8154c92609a26dd
magat <mathieu.agar@gmail.com>
2015-07-15 14:58:39 +0200

    WIP: Player shoots the ball

--- demo.js
+++ demo.js
@@ -24,40 +24,43 @@ ball.freeze();
 sky.addChild(ball.avatar);
 
 var player = createPlayer();
-player.place(20, 500);
+player.place(W/2 - 25, 500);
 sky.addChild(player.avatar);
 var bigBounds;
 function loop(){
-	requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 
-	ball.refresh();
-	player.refresh();
+  ball.refresh();
+  player.refresh();
 
   // compute collisions between the ball and the player
   var playerBounds = player.avatar.getBounds();
   bigBounds = new PIXI.Rectangle(playerBounds.x - BALL_RADIUS, playerBounds.y - BALL_RADIUS, playerBounds.width + BALL_RADIUS *2, playerBounds.height + BALL_RADIUS * 2);
   if(bigBounds.contains(ball.position.x, ball.position.y)){
-    console.log("HIT THE BALL");
     ball.release();
+
     // compute the force applied to the ball
     var dir = new PIXI.Point(0,0);
     if(player.position.y > ball.position.y){
       dir.y = -10;
-    }
+    } else {
 
-    // x of the center of the player
-    var x = player.position.x + player.avatar.width/2;
-    if(x < ball.position.x){
-      dir.x = 1;
-    } 
-    if(x > ball.position.x){
-      dir.x = -1;
+      // ball hit the left side 
+      if(ball.position.x <= bigBounds.x){
+        dir.x = -10;
+      } 
+      // ball hit the right side 
+      if(ball.position.x >= bigBounds.x + BALL_RADIUS *2){
+        dir.x = 10;
+      }
     }
+    dir.x += player.velocity.x;
+    dir.y += player.velocity.y;
 
     ball.push(dir.x, dir.y);
   } 
 
-	renderer.render(stage);
+  renderer.render(stage);
 }
 
 loop();
